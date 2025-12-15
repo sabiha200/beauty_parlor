82e44eafc61aaa677ef5c11f3a51a51116a47dff
silma subah <silmasubah105@gmail.com>
2025-12-15 20:13:42 +0600

    msg

--- js/script.js
+++ js/script.js
@@ -1,21 +1,83 @@
+//all button
 let activebtn= document.querySelectorAll(".category-btn")
-console.log(activebtn)
+// all Card
+let card= document.querySelectorAll(".service-card")
+let servecard= document.querySelector(".services-grid");
+
 
 
 activebtn.forEach((item)=>(
 
     item.addEventListener('click' ,()=>{
+        
+      activebtn.forEach((b)=> b.classList.remove("active"))
         item.classList.toggle("active")
+       
+        card.forEach((el)=>{
+          if(item.classList[1]=="allservice"){
+             card.forEach((card)=>{
+               card.style="display:block"
+             })
+          }
+          if((item.classList[1] == el.classList[1]) && item.classList[2]=="active"){
+             el.style="display:block"
+             servecard.append(el)
+             console.log(servecard)
+          }else {
+             el.style="display:none"
+          }
+        
+        
+        })
     })
+
+
 ))
 
 
- $(document).ready(function(){
+// slick js start //
     $('.mentor').slick({
       dots: false,
       infinite: true,
       speed: 300,
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      prevArrow:`<button class="leftarr"><i class="fa-solid fa-angle-left"></i></button>`,
+      nextArrow:`<button class="rightarr"><i class="fa-solid fa-angle-right"></i></button>`,
+      responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+     
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
     });
-  });
+ 
+//slick js end //
